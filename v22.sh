84a34f65959ce2195e98e45bcf84b11f2df6e132
amoshi <amoshi.mandrakeuser@gmail.com>
2018-05-05 14:45:40 +0300

    fix v22.sh

--- v22.sh
+++ v22.sh
@@ -7,8 +7,9 @@ SEARCHMASK="$2"
 
 [ -z "$2" ] && echo "No search mask" && exit 2
 
+cd $DIRNAME
 mkdir tmp
-LISTFILES=`ls -al $DIRNAME | grep '^-' | grep $SEARCHMASK | awk '{print $9}'`
+LISTFILES=`ls -al | grep '^-' | grep $SEARCHMASK | awk '{print $9}'`
 
 for FILE in $LISTFILES
 do
@@ -17,5 +18,7 @@ do
 	cat $FILE | awk '{if (!h[$0]) h[$0]=NR} END {for (k in h) {print h[k],k}}' | sort -k1n | cut -d' ' -f2- > $tmpfile
 	mv $tmpfile $FILE
 done
+rm -rf tmp
+cd -
 
 echo $LISTFILES
