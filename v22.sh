3651a98ca229f0eb740bf1c54a13e44695f47e5f
amoshi <amoshi.mandrakeuser@gmail.com>
2018-05-05 14:36:47 +0300

    add v22.sh

--- v22.sh
+++ v22.sh
@@ -8,11 +8,11 @@ SEARCHMASK="$2"
 [ -z "$2" ] && echo "No search mask" && exit 2
 
 mkdir tmp
-LISTFILES=`ls -al | grep '^-' | grep $SEARCHMASK | awk '{print $9}'`
+LISTFILES=`ls -al $DIRNAME | grep '^-' | grep $SEARCHMASK | awk '{print $9}'`
 
 for FILE in $LISTFILES
 do
-	tmpfile=$(mktemp tmp/$FILE.XXXXXX)
+	tmpfile=`mktemp tmp/$FILE.XXXXXX`
 	#sort -u $FILE > $tmpfile
 	cat $FILE | awk '{if (!h[$0]) h[$0]=NR} END {for (k in h) {print h[k],k}}' | sort -k1n | cut -d' ' -f2- > $tmpfile
 	mv $tmpfile $FILE
